cc6bb38a4dc6e175bca61919427572879d524b0f
hexs00si <shravanr.biz@gmail.com>
2024-05-18 15:52:36 +0530

    Created a mininav component mininav component created

---
+++ Entertainment-live/src/App.jsx
@@ -0,0 +1,11 @@
+import Mininav from "./components/Mininav"
+
+const App = () => {
+  return (
+   <>
+   <Mininav />
+   </>
+  )
+}
+
+export default App
---
+++ Entertainment-live/src/components/SecondMininav.jsx
@@ -0,0 +1,5 @@
+function SecondMininav() {
+  return <></>;
+}
+
+export default SecondMininav;
