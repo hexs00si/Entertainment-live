bd2abbb5973b21d487994db7a494b7106bc70155
hexs00si <shravanr.biz@gmail.com>
2024-05-19 13:46:06 +0530

    created the latestvideo component and added css accordingly

--- Entertainment-live/src/App.jsx
+++ Entertainment-live/src/App.jsx
@@ -1,4 +1,5 @@
 // import Card from "./components/Card"
+import Latestvideos from "./components/Latestvideos"
 import Mininav from "./components/Mininav"
 // import Newswindow from "./components/Newswindow"
 // import Pollcard from "./components/Pollcard"
@@ -8,7 +9,7 @@ const App = () => {
   return (
    <>
    <Mininav />
-   
+   <Latestvideos />
    </>
   )
 }
---
+++ Entertainment-live/src/components/Latestvideos.jsx
@@ -0,0 +1,45 @@
+import saif from '../assets/images/Layer 16.png'
+import man from '../assets/images/Layer 21.png'
+
+const Latestvideos = () => {
+  return (
+    <div className='flex flex-row  w-[45%] mx-auto items-start videocardBg rounded-xl p-3 gap-4'>
+        <img className='rounded-xl h-full object-cover' src={saif} alt="" />
+        <div className='flex flex-col gap-1.5'>
+        <div className='flex items-start vidRow rounded gap-0.5 p-0.5'>
+                <img className='mr-2.5' src={man} alt='' />
+                <article className='flex flex-col gap-0 mr-3'>
+                    <h3 className='text-s font-bold font-sans'>STRING MASTER</h3>
+                    <p className='text-xs font-light font-sans '>Electric Ganesha Land</p>
+                </article>
+            </div>
+            <div className='flex items-start vidRow rounded gap-0.5 p-0.5'>
+                <img className='mr-2.5' src={man} alt='' />
+                <article className='flex flex-col gap-0 mr-3'>
+                    <h3 className='text-s font-bold font-sans'>STRING MASTER</h3>
+                    <p className='text-xs font-light font-sans '>Electric Ganesha Land</p>
+                </article>
+            </div>
+            <div className='flex items-start vidRow rounded gap-0.5 p-0.5'>
+                <img className='mr-2.5' src={man} alt='' />
+                <article className='flex flex-col gap-0 mr-3'>
+                    <h3 className='text-s font-bold font-sans'>STRING MASTER</h3>
+                    <p className='text-xs font-light font-sans '>Electric Ganesha Land</p>
+                </article>
+            </div>
+            <div className='flex items-start vidRow rounded gap-0.5 p-0.5'>
+                <img className='mr-2.5' src={man} alt='' />
+                <article className='flex flex-col gap-0 mr-3'>
+                    <h3 className='text-s font-bold font-sans'>STRING MASTER</h3>
+                    <p className='text-xs font-light font-sans '>Electric Ganesha Land</p>
+                </article>
+            </div> 
+        </div>
+    </div>
+  )
+}
+
+export default Latestvideos
+
+
+// populate the videos using Youtube data api and have a working media player for this 
