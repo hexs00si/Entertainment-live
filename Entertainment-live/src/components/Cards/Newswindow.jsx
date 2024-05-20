e3b02696d0514da3727d7ea0f125338344435f30
hexs00si <shravanr.biz@gmail.com>
2024-05-20 13:18:11 +0530

    still having the deployment issue

--- Entertainment-live/src/components/Cards/Newswindow.jsx
+++ Entertainment-live/src/components/Cards/Newswindow.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import redbtn from '/src/assets/images/Layer 4 copy 4.png';
+import { useEffect, useState } from "react";
+import redbtn from "/src/assets/images/Layer 4 copy 4.png";
 import newsimage from "/src/assets/images/Layer 14.png";
 
 const Newswindow = () => {
@@ -11,8 +11,8 @@ const Newswindow = () => {
       "https://newsapi.org/v2/top-headlines?country=us&apiKey=72006d00cece4909b82b68131df9dc66"
     );
     let data = await response.json();
-    setMyNews(data.articles?.slice(0, 6));  // Get only the first 6 articles
-    setSelectedArticle(data.articles[0]);  // Set the first article as the default selected article
+    setMyNews(data.articles.slice(0, 6)); // Get only the first 6 articles
+    setSelectedArticle(data.articles[0]); // Set the first article as the default selected article
   };
 
   useEffect(() => {
@@ -31,7 +31,7 @@ const Newswindow = () => {
         )}
         <div className="absolute bottom-0 left-0 w-full h-1/3 bg-newscap bg-opacity-75 px-3 py-1 mb-3 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out pointer-events-none group-hover:pointer-events-auto rounded-lg flex flex-col justify-between">
           <p className="text-sm text-white font-sans overflow-scroll text-ellipsis whitespace-normal">
-            {selectedArticle ? selectedArticle.description : 'Loading...'}
+            {selectedArticle ? selectedArticle.description : "Loading..."}
           </p>
           <button className="btn font-sans font-semibold text-white text-xs mt-2 items-end">
             Read More
@@ -43,17 +43,25 @@ const Newswindow = () => {
           Latest <span className="text-navbackground">Articles</span>
         </h2>
         <ul className="newsWindowul rounded-xl flex flex-col gap-2 w-full">
-          {mynews.slice(1, 6).map((article, index) => (  // Get the next 5 articles
-            <li key={index} className="rounded-xl transition-transform duration-300 ease-in-out hover:scale-105 hover:cursor-pointer"
-                onClick={() => setSelectedArticle(article)}>
-              <span className="flex gap-2">
-                <img className="size-3 ml-2" src={redbtn} alt="" />
-                <p className="font-sans text-xs font-light">
-                  {article.title}
-                </p>
-              </span>
-            </li>
-          ))}
+          {mynews.slice(1, 6).map(
+            (
+              article,
+              index // Get the next 5 articles
+            ) => (
+              <li
+                key={index}
+                className="rounded-xl transition-transform duration-300 ease-in-out hover:scale-105 hover:cursor-pointer"
+                onClick={() => setSelectedArticle(article)}
+              >
+                <span className="flex gap-2">
+                  <img className="size-3 ml-2" src={redbtn} alt="" />
+                  <p className="font-sans text-xs font-light">
+                    {article.title}
+                  </p>
+                </span>
+              </li>
+            )
+          )}
         </ul>
       </div>
     </div>
